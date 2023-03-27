603f24a953700ec87511ec10da94acc41d949c1d
Kirill <K.katyshev1503@gmail.com>
2023-03-27 17:40:51 +0300

    FirstCommit

---
+++ src/main/java/com/example/schoolhofwarts/SchoolHofwartsApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolhofwarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolHofwartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolHofwartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schoolhofwarts/SchoolHofwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolhofwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolHofwartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
